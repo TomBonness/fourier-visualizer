dcfabcd35c0830886c8537516535fa3b4d9165d0
Tom Bonness <105823687+TomBonness@users.noreply.github.com>
2025-08-10 22:06:22 -0500

    added second rotating circle

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,23 @@ int main() {
 
     std::cout << "Fourier Visualizer - Window created!" << std::endl;
 
-    // Create a simple circle
-    sf::CircleShape circle(20.f);  // radius of 20
-    circle.setFillColor(sf::Color::White);
-    circle.setOrigin({20.f, 20.f});  // set origin to center of circle
+    // Create first circle
+    sf::CircleShape circle1(20.f);
+    circle1.setFillColor(sf::Color::White);
+    circle1.setOrigin({20.f, 20.f});
+
+    // Create second circle
+    sf::CircleShape circle2(15.f);
+    circle2.setFillColor(sf::Color::White);
+    circle2.setOrigin({15.f, 15.f});
 
     // Animation variables
     sf::Clock clock;
     float time = 0.f;
     float centerX = 640.f;
     float centerY = 360.f;
-    float orbitRadius = 150.f;
+    float orbitRadius1 = 150.f;
+    float orbitRadius2 = 80.f;
 
     // Main loop
     while (window.isOpen()) {
@@ -34,17 +40,24 @@ int main() {
             }
         }
 
-        // Update circle position (rotate around center)
-        float angle = time;
-        float x = centerX + orbitRadius * std::cos(angle);
-        float y = centerY + orbitRadius * std::sin(angle);
-        circle.setPosition({x, y});
+        // Update first circle position (rotate around center)
+        float angle1 = time;
+        float x1 = centerX + orbitRadius1 * std::cos(angle1);
+        float y1 = centerY + orbitRadius1 * std::sin(angle1);
+        circle1.setPosition({x1, y1});
+
+        // Update second circle position (rotate around first circle)
+        float angle2 = time * 2.5f;  // faster rotation
+        float x2 = x1 + orbitRadius2 * std::cos(angle2);
+        float y2 = y1 + orbitRadius2 * std::sin(angle2);
+        circle2.setPosition({x2, y2});
 
         // Clear with black background
         window.clear(sf::Color::Black);
 
-        // Draw the circle
-        window.draw(circle);
+        // Draw both circles
+        window.draw(circle1);
+        window.draw(circle2);
 
         // Display
         window.display();
